a5e93c1d5482b838afc018099cc9dca64e68006d
Mihir Mahakalkar <mihir.mahakalkar@gmail.com>
2025-04-20 00:53:25 +0530

    dummy

--- scripts/scan.sh
+++ scripts/scan.sh
@@ -1,3 +1,4 @@
+
 printf "\n====== SCANNING FOR CREDIT CARD NUMBERS ======\n"
 grep -rE --color=always '(\b[0-9]{4}[- ]?){3}[0-9]{4}\b' . --exclude-dir={.git} --line-number
 echo "========= CREDIT CARD SCAN COMPLETE =========="
